0e8536274946209403e68a39c0aa3dce24d9a694
alaa shaaban <alaa.shaaban943@gmail.com>
2026-04-16 09:31:49 +0200

    feat: handle reset password flow and display form based on access token

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+'use client';
+import { useEffect } from 'react';
+
 export default function Home() {
-  return <div className="text-primary">hi</div>;
+  useEffect(() => {
+    const hash = window.location.hash.substring(1);
+    const params = new URLSearchParams(hash);
+    const type = params.get('type');
+    const error = params.get('error');
+    if (type === 'recovery' || error) {
+      window.location.href = `/reset-password#${hash}`;
+    }
+  }, []);
+  return <div className="text-primary">Home Page</div>;
 }
---
+++ src/app/reset-password/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { ResetPasswordForm } from '../../components/forms/ResetPasswordForm';
+export default function ResetPassword() {
+  const hash =
+    typeof window !== 'undefined' ? window.location.hash.substring(1) : '';
+  const params = new URLSearchParams(hash);
+  const token = params.get('access_token');
+  const error = params.get('error');
+  return (
+    <div>
+      {!token || error ? (
+        <div className="flex items-center justify-center min-h-screen text-error">
+          <p className="text-center text-red-500 text-lg font-medium">Invalid or expired reset link.</p>
+        </div>
+      ) : (
+        <ResetPasswordForm />
+      )}
+    </div>
+  );
+}
