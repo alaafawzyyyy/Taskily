3fdabdcd4b44c444a7bb660b8156e6d3a4980ae5
alaa shaaban <alaa.shaaban943@gmail.com>
2026-04-20 15:59:54 +0200

    feat: Add project form validation

--- src/components/forms/AddProjectForm.tsx
+++ src/components/forms/AddProjectForm.tsx
@@ -1,10 +1,50 @@
+'use client';
 import Image from 'next/image';
 import right from '../../../public/assets/icons/right.svg';
+import errormark from '../../../public/assets/icons/errormark.svg';
 import protip from '../../../public/assets/icons/protip .svg';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm, useWatch } from 'react-hook-form';
+import z from 'zod';
+
+// React Form Hook and Validation
+const addProjectSchema = z.object({
+  name: z
+    .string()
+    .min(3, 'Project name must be at least 3 characters.')
+    .max(100, 'Project name must be less than 100 characters.'),
+  description: z
+    .string()
+    .max(500, 'Description must be less than 500 character')
+    .optional(),
+});
+
+type formType = z.infer<typeof addProjectSchema>;
 
 export function AddProjectForm() {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<formType>({ resolver: zodResolver(addProjectSchema) });
+
+  const description = useWatch({
+    control,
+    name: 'description',
+    defaultValue: '',
+  });
+  const descriptionLength = (description ?? '').length;
+
+  const submitForm = async (data: formType) => {
+    console.log(data);
+  };
+
   return (
-    <div className="flex flex-col md:top-[60px] md:left-[32px] rounded-2 md:p-4 relative md:items-center px-6 pt-8 pb-[80px] md:max-w-[960px] w-full">
+    <form
+      onSubmit={handleSubmit(submitForm)}
+      className="flex flex-col md:top-[60px] md:left-[32px] rounded-2 md:p-4 relative md:items-center px-6 pt-8 pb-[80px] md:max-w-[960px] w-full"
+    >
       <div className="flex flex-col md:rounded-2 md:bg-white w-full md:max-w-[672px] ">
         {/* Form Header */}
         <div className="md:border-b pb-10 md:px-8 pt-8 md:border-[#F1F3FF]">
@@ -36,10 +76,19 @@ export function AddProjectForm() {
             </p>
             <input
               type="text"
+              {...register('name')}
               className="rounded-[4px] py-3 px-4 bg-[#D7E2FF] border-2 border-[#000000]"
             />
-            <p className="text-[12px] font-medium leading-4 text-[#BA1A1A]">
-              Error
+            <p className=" flex gap-1 text-[12px] font-medium leading-4 text-[#BA1A1A]">
+              {errors.name ? (
+                <Image
+                  src={errormark}
+                  alt="Error mark"
+                />
+              ) : (
+                ''
+              )}
+              {errors.name?.message}
             </p>
           </div>
 
@@ -55,10 +104,25 @@ export function AddProjectForm() {
               className="rounded-[4px] pt-3 pb-[84px] px-4 bg-[#D7E2FF] border-2 border-[#000000] placeholder: text-[16px] text-[#4F5F7B] leading-6 "
               placeholder="Provide a high-level overview of the project's architectural
                objectives and key milestones..."
+              {...register('description')}
+              maxLength={500}
             />
-            <p className="text-end text-[11px] font-medium text-[#4F5F7B] leading-[16.5px]">
-              0 / 500 character
-            </p>
+            <div className="flex justify-between">
+              <p className=" flex gap-1 text-[12px] font-medium leading-4 text-[#BA1A1A]">
+                {errors.description ? (
+                  <Image
+                    src={errormark}
+                    alt="Error mark"
+                  />
+                ) : (
+                  ''
+                )}
+                {errors.description?.message}
+              </p>
+              <p className="text-end text-[11px] font-medium text-[#4F5F7B] leading-[16.5px]">
+                {descriptionLength} / 500 character
+              </p>
+            </div>
           </div>
 
           {/* Cancel && Create */}
@@ -72,19 +136,21 @@ export function AddProjectForm() {
           </div>
         </div>
         {/* Footer Pro Tip */}
-          <div className="flex pt-12 md:p-6 gap-3 md:bg-[#F1F3FF]">
-            <Image
-              src={protip}
-              alt="Pro tip icon"
-              className="hidden md:block"
-            />
-            <p className="text-[12px] leading-[19.5px] text-[#4F5F7B]">
-              <span className="block md:inline text-[12px] font-bold ">Pro Tip: </span> You can
-              invite project members and assign epics immediately after the
-              initial creation process.
-            </p>
+        <div className="flex pt-12 md:p-6 gap-3 md:bg-[#F1F3FF]">
+          <Image
+            src={protip}
+            alt="Pro tip icon"
+            className="hidden md:block"
+          />
+          <p className="text-[12px] leading-[19.5px] text-[#4F5F7B]">
+            <span className="block md:inline text-[12px] font-bold ">
+              Pro Tip:{' '}
+            </span>{' '}
+            You can invite project members and assign epics immediately after
+            the initial creation process.
+          </p>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
